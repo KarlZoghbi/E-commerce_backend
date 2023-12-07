84038706c4bb581ee0487a4dc1b20df4e9e185f5
TheKuxx <Karlzoghbi1@gmail.com>
2023-12-07 22:17:08 +0200

    fixed-user-type-id

--- connection.php
+++ connection.php
@@ -1,17 +1,28 @@
 <?php
 
-header ("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Content-Type');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 $host = "localhost";
 $db_user = "root";
 $db_pass = null;
-$db_name = "e_commerce";
+$db_name = "e-commerce";
 
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
+} else {
+    echo "Connected successfully\n";
 }
+
+$check_db_query = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db_name'";
+$result = $mysqli->query($check_db_query);
+
+if ($result->num_rows == 0) {
+    die("Database '$db_name' does not exist.\n");
+}
+
+
 ?>
--- signup.php
+++ signup.php
@@ -12,13 +12,16 @@ if (isset($_POST['email'], $_POST['password'], $_POST['user_name'], $_POST['firs
 
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $query = $mysqli->prepare('INSERT INTO users (email, password, user_name, first_name, last_name, age, gender) VALUES (?, ?, ?, ?, ?, ?, ?)');
-    
+    // Set the user_type_id to '1' for a regular user
+    $user_type_id = 1;
+
+    $query = $mysqli->prepare('INSERT INTO users (email, password, user_name, first_name, last_name, age, gender, user_type_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)');
+
     if (!$query) {
         die("Error in query preparation: " . $mysqli->error);
     }
 
-    $query->bind_param('sssssis', $email, $hashed_password, $user_name, $first_name, $last_name, $age, $gender);
+    $query->bind_param('sssssisi', $email, $hashed_password, $user_name, $first_name, $last_name, $age, $gender, $user_type_id);
     $query->execute();
 
     if ($query->error) {
